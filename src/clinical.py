9ce728c9e1d611d8f15fc9f83b2843ae554a45e7
Sergio <sdepineresramirez@utb.edu.co>
2023-09-05 12:33:47 -0500

    created class for clinical records

---
+++ src/clinical.py
@@ -0,0 +1,65 @@
+from evonotes import EvoNotes
+from diagnostic import DiagnosticImages
+from prescriptions import Prescriptions
+from labresults import LabResults
+
+
+class ClinicalRecords():
+    def __init__(self, evolution_notes: EvoNotes = EvoNotes(),
+                 diagnostic_images: DiagnosticImages = DiagnosticImages(),
+                 lab_results: LabResults = LabResults(),
+                 prescriptions: Prescriptions = Prescriptions()):
+        '''
+        Constructor of the class ClinicalRecords class
+
+
+        :param evolution_notes: evolution notes of the patient
+        :type evolution_notes: EvoNotes
+        :param diagnostic_images: diagnostic images of the patient
+        :type diagnostic_images: DiagnosticImages
+        :param lab_results: lab results of the patient
+        :type lab_results: LabResults
+        '''
+        self.__evolution_notes = evolution_notes
+        self.__diagnostic_images = diagnostic_images
+        self.__lab_results = lab_results
+        self.__prescriptions = prescriptions
+
+    @ property
+    def evolution_notes(self):
+        '''
+        Returns the evolution_notes attribute
+
+        :return: evolution notes of the patient
+        :rtype: EvoNotes
+        '''
+        return self.__evolution_notes
+
+    @ property
+    def diagnostic_images(self):
+        '''
+        Returns the diagnostic_images attribute
+
+        :return: diagnostic images of the patient
+        :rtype: DiagnosticImages
+        '''
+        return self.__diagnostic_images
+
+    @ property
+    def lab_results(self):
+        '''
+        Returns the lab_results attribute
+
+        :return: lab results of the patient
+        :rtype: LabResults
+        '''
+        return self.__lab_results
+
+    def prescriptions(self):
+        '''
+        Returns the prescriptions attribute
+
+        :return: prescriptions of the patient
+        :rtype: Prescription
+        '''
+        return self.__prescriptions
